0c1c313ff921857ecf9300948c2dee63cfba79a5
Haryanto Quang <quang.haryanto@gmail.com>
2020-07-03 17:22:56 +0700

    Initial Commit

---
+++ CountryInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountryInfo
+//
+//  Created by Haryanto Salim on 03/07/20.
+//  Copyright © 2020 Haryanto Salim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
